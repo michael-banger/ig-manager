5d86df79a057881e4f443e6d920c2598afbaf4f7
michael-banger <yourtechnologybso@gmail.com>
2026-05-23 22:39:22 +0200

    Fix mobile post action buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ig-manager-v1';
+const CACHE = 'ig-manager-v2';
 const ASSETS = ['/ig-manager/', '/ig-manager/index.html', '/ig-manager/manifest.json', '/ig-manager/icon.svg'];
 
 self.addEventListener('install', e => {
